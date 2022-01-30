303a0d974d136ccf946a154e7f02e6cb5c20bb43
Kyle Ranous <kranous@Concept32designs.com>
2022-01-29 20:08:06 -0700

    Created initial route for get_recipe API

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, jsonify
 from app import app
+from app.models import Recipe, Ingredient
 
 
 @app.route('/')
@@ -15,4 +16,26 @@ def get_all_recipes():
 
 @app.route('/api/recipe/<int:id>', methods=['GET'])
 def get_recipe(id):
-    pass
+    # Find the Recipe with matching ID
+    try:
+        r = Recipe.query.get(id).to_dict()
+    
+    except: # If Recipe can't be found, return the error message
+        return jsonify({"error": f"No Recipe with id:{id}"})
+
+    # If the Recipe has been found query the ingredients
+    try:    
+        i = Ingredient.query.filter_by(recipe_id=id).all()
+
+    except: # If no ingredients found - return error message
+        return jsonify({"error": f"No Ingredients for Recipe {id}"})
+
+    # Add Ingredients to recipe dict
+    ingredients = []    
+    for item in i:
+        ingredients.append(item.to_dict())
+    r["ingredients"] = ingredients
+    print(r["ingredients"])
+    # If the Recipe and Ingredients have been found query the steps
+
+    return jsonify(r)
